268ceb730ec4f53a23d71dda6c79bb7033dbbdd5
gustavo.simao <gustuevo@outlook.com>
2023-11-03 09:41:29 -0300

    Finalzando parte de busca de links por arquivo e diretorio

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,24 @@
 import retornaLinks from './index.js';
-
+import fs from 'fs';
 const caminho = process.argv;
 
+function imprimeLista(lista) {
+    console.log(lista);
+}
+
 async function processaTexto(argumentos) {
-    const resultado = await retornaLinks(argumentos[2]);
-    console.log(resultado);
+    const caminho = argumentos[2];
+
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await retornaLinks(caminho);
+        imprimeLista(resultado);
+
+    } else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho);
+        arquivos.forEach(async (arquivo) => {
+            const conteudo = await retornaLinks(`${caminho}/${arquivo}`);
+            imprimeLista(conteudo);
+        })
+    }
 }
 processaTexto(caminho);
